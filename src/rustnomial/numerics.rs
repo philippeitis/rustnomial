570654c369487e85e756cca863a92d70cccfd85a
philippeitis <33013301+philippeitis@users.noreply.github.com>
2020-07-31 21:30:55 -0700

    Add AbsSqrt to numerics.rs

--- src/rustnomial/numerics.rs
+++ src/rustnomial/numerics.rs
@@ -331,3 +331,403 @@ impl IsPositive for f64 {
         self > &0.0
     }
 }
+
+pub trait AbsSqrt {
+    fn abs_sqrt(self) -> Self;
+}
+
+impl AbsSqrt for u8 {
+    fn abs_sqrt(self) -> Self {
+        let x = self;
+        if x < 2 {
+            return x;
+        }
+
+        let shift = 8 - x.leading_zeros();
+
+        let mut guess = (x >> (shift / 2)) + 1;
+        let mut res = (guess + x / guess) / 2;
+        loop {
+            if res > guess {
+                if res - guess <= 1 {
+                    break;
+                }
+            } else {
+                if guess - res <= 1 {
+                    break;
+                }
+            }
+            guess = res;
+            res = (guess + x / guess) / 2;
+        }
+
+        while res * res > x {
+            res -= 1
+        }
+        res
+    }
+}
+
+impl AbsSqrt for u16 {
+    fn abs_sqrt(self) -> Self {
+        let x = self;
+        if x < 2 {
+            return x;
+        }
+
+        let shift = 16 - x.leading_zeros();
+
+        let mut guess = (x >> (shift / 2)) + 1;
+        let mut res = (guess + x / guess) / 2;
+        loop {
+            if res > guess {
+                if res - guess <= 1 {
+                    break;
+                }
+            } else {
+                if guess - res <= 1 {
+                    break;
+                }
+            }
+            guess = res;
+            res = (guess + x / guess) / 2;
+        }
+
+        while res * res > x {
+            res -= 1
+        }
+        res
+    }
+}
+
+impl AbsSqrt for u32 {
+    fn abs_sqrt(self) -> Self {
+        let x = self;
+        if x < 2 {
+            return x;
+        }
+
+        let shift = 32 - x.leading_zeros();
+
+        let mut guess = (x >> (shift / 2)) + 1;
+        let mut res = (guess + x / guess) / 2;
+        loop {
+            if res > guess {
+                if res - guess <= 1 {
+                    break;
+                }
+            } else {
+                if guess - res <= 1 {
+                    break;
+                }
+            }
+            guess = res;
+            res = (guess + x / guess) / 2;
+        }
+
+        while res * res > x {
+            res -= 1
+        }
+        res
+    }
+}
+
+impl AbsSqrt for u64 {
+    fn abs_sqrt(self) -> Self {
+        let x = self;
+        if x < 2 {
+            return x;
+        }
+
+        let shift = 64 - x.leading_zeros();
+
+        let mut guess = (x >> (shift / 2)) + 1;
+        let mut res = (guess + x / guess) / 2;
+        loop {
+            if res > guess {
+                if res - guess <= 1 {
+                    break;
+                }
+            } else {
+                if guess - res <= 1 {
+                    break;
+                }
+            }
+            guess = res;
+            res = (guess + x / guess) / 2;
+        }
+
+        while res * res > x {
+            res -= 1
+        }
+        res
+    }
+}
+
+impl AbsSqrt for u128 {
+    fn abs_sqrt(self) -> Self {
+        let x = self;
+        if x < 2 {
+            return x;
+        }
+
+        let shift = 128 - x.leading_zeros();
+
+        let mut guess = (x >> (shift / 2)) + 1;
+        let mut res = (guess + x / guess) / 2;
+        loop {
+            if res > guess {
+                if res - guess <= 1 {
+                    break;
+                }
+            } else {
+                if guess - res <= 1 {
+                    break;
+                }
+            }
+            guess = res;
+            res = (guess + x / guess) / 2;
+        }
+
+        while res * res > x {
+            res -= 1
+        }
+        res
+    }
+}
+
+impl AbsSqrt for usize {
+    fn abs_sqrt(self) -> Self {
+        let x = self;
+        if x < 2 {
+            return x;
+        }
+
+        let shift = 0usize.count_zeros() - x.leading_zeros();
+
+        let mut guess = (x >> (shift / 2)) + 1;
+        let mut res = (guess + x / guess) / 2;
+        loop {
+            if res > guess {
+                if res - guess <= 1 {
+                    break;
+                }
+            } else {
+                if guess - res <= 1 {
+                    break;
+                }
+            }
+            guess = res;
+            res = (guess + x / guess) / 2;
+        }
+
+        while res * res > x {
+            res -= 1
+        }
+        res
+    }
+}
+
+impl AbsSqrt for i8 {
+    fn abs_sqrt(self) -> Self {
+        let x = self.abs();
+        if x < 2 {
+            return x;
+        }
+
+        let shift = 8 - x.leading_zeros();
+
+        let mut guess = (x >> (shift / 2)) + 1;
+        let mut res = (guess + x / guess) / 2;
+        loop {
+            if res > guess {
+                if res - guess <= 1 {
+                    break;
+                }
+            } else {
+                if guess - res <= 1 {
+                    break;
+                }
+            }
+            guess = res;
+            res = (guess + x / guess) / 2;
+        }
+
+        while res * res > x {
+            res -= 1
+        }
+        res
+    }
+}
+
+impl AbsSqrt for i16 {
+    fn abs_sqrt(self) -> Self {
+        let x = self.abs();
+        if x < 2 {
+            return x;
+        }
+
+        let shift = 16 - x.leading_zeros();
+
+        let mut guess = (x >> (shift / 2)) + 1;
+        let mut res = (guess + x / guess) / 2;
+        loop {
+            if res > guess {
+                if res - guess <= 1 {
+                    break;
+                }
+            } else {
+                if guess - res <= 1 {
+                    break;
+                }
+            }
+            guess = res;
+            res = (guess + x / guess) / 2;
+        }
+
+        while res * res > x {
+            res -= 1
+        }
+        res
+    }
+}
+
+impl AbsSqrt for i32 {
+    fn abs_sqrt(self) -> Self {
+        let x = self.abs();
+        if x < 2 {
+            return x;
+        }
+
+        let shift = 32 - x.leading_zeros();
+
+        let mut guess = (x >> (shift / 2)) + 1;
+        let mut res = (guess + x / guess) / 2;
+        loop {
+            if res > guess {
+                if res - guess <= 1 {
+                    break;
+                }
+            } else {
+                if guess - res <= 1 {
+                    break;
+                }
+            }
+            guess = res;
+            res = (guess + x / guess) / 2;
+        }
+
+        while res * res > x {
+            res -= 1
+        }
+        res
+    }
+}
+
+impl AbsSqrt for i64 {
+    fn abs_sqrt(self) -> Self {
+        let x = self.abs();
+        if x < 2 {
+            return x;
+        }
+
+        let shift = 64 - x.leading_zeros();
+
+        let mut guess = (x >> (shift / 2)) + 1;
+        let mut res = (guess + x / guess) / 2;
+        loop {
+            if res > guess {
+                if res - guess <= 1 {
+                    break;
+                }
+            } else {
+                if guess - res <= 1 {
+                    break;
+                }
+            }
+            guess = res;
+            res = (guess + x / guess) / 2;
+        }
+
+        while res * res > x {
+            res -= 1
+        }
+        res
+    }
+}
+
+impl AbsSqrt for i128 {
+    fn abs_sqrt(self) -> Self {
+        let x = self.abs();
+        if x < 2 {
+            return x;
+        }
+
+        let shift = 128 - x.leading_zeros();
+
+        let mut guess = (x >> (shift / 2)) + 1;
+        let mut res = (guess + x / guess) / 2;
+        loop {
+            if res > guess {
+                if res - guess <= 1 {
+                    break;
+                }
+            } else {
+                if guess - res <= 1 {
+                    break;
+                }
+            }
+            guess = res;
+            res = (guess + x / guess) / 2;
+        }
+
+        while res * res > x {
+            res -= 1
+        }
+        res
+    }
+}
+
+impl AbsSqrt for isize {
+    fn abs_sqrt(self) -> Self {
+        let x = self.abs();
+        if x < 2 {
+            return x;
+        }
+
+        let shift = 0isize.count_zeros() - x.leading_zeros();
+
+        let mut guess = (x >> (shift / 2)) + 1;
+        let mut res = (guess + x / guess) / 2;
+        loop {
+            if res > guess {
+                if res - guess <= 1 {
+                    break;
+                }
+            } else {
+                if guess - res <= 1 {
+                    break;
+                }
+            }
+            guess = res;
+            res = (guess + x / guess) / 2;
+        }
+
+        while res * res > x {
+            res -= 1
+        }
+        res
+    }
+}
+
+impl AbsSqrt for f32 {
+    fn abs_sqrt(self) -> Self {
+        self.abs().sqrt()
+    }
+}
+
+impl AbsSqrt for f64 {
+    fn abs_sqrt(self) -> Self {
+        self.abs().sqrt()
+    }
+}
