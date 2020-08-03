a076fa99d8edabaac943340c729a16b346beb550
philippeitis <33013301+philippeitis@users.noreply.github.com>
2020-08-02 18:45:07 -0700

    Use macros for poly_math.rs

--- src/rustnomial/poly_math.rs
+++ src/rustnomial/poly_math.rs
@@ -1,59 +1,69 @@
-use num::Zero;
-use rustnomial::traits::FreeSizePolynomial;
-use std::ops::{AddAssign, Mul, Neg};
-use {GenericPolynomial, Polynomial};
-
-pub fn poly_add<N>(
-    src_a: Box<dyn GenericPolynomial<N>>,
-    src_b: Box<dyn GenericPolynomial<N>>,
-) -> Polynomial<N>
-where
-    N: Zero + Copy + AddAssign,
-{
-    let mut sink = Polynomial::zero();
-    for (coeff, deg) in src_a.term_iter() {
-        sink.add_term(coeff, deg);
-    }
-
-    for (coeff, deg) in src_b.term_iter() {
-        sink.add_term(coeff, deg);
-    }
-
-    sink
-}
-
-pub fn poly_mul<N>(
-    src_a: Box<dyn GenericPolynomial<N>>,
-    src_b: Box<dyn GenericPolynomial<N>>,
-) -> Polynomial<N>
-where
-    N: Zero + Copy + AddAssign + Mul<Output = N>,
-{
-    let mut sink = Polynomial::zero();
-    for (coeff_a, deg_a) in src_a.term_iter() {
-        for (coeff_b, deg_b) in src_b.term_iter() {
-            sink.add_term(coeff_a * coeff_b, deg_a + deg_b);
+#[macro_export]
+macro_rules! poly_add {
+    ($lhs:expr, $rhs:expr) => {
+        {
+            let mut sink = Polynomial::zero();
+            poly_add!($lhs, $rhs, sink)
         }
-    }
+    };
 
-    sink
+    ($lhs:expr, $rhs:expr, $sink:expr) => {
+        {
+        for (coeff, deg) in $lhs.term_iter() {
+            $sink.add_term(coeff, deg);
+        }
+
+        for (coeff, deg) in $rhs.term_iter() {
+            $sink.add_term(coeff, deg);
+        }
+
+        $sink
+        }
+    };
 }
 
-pub fn poly_sub<N>(
-    src_a: Box<dyn GenericPolynomial<N>>,
-    src_b: Box<dyn GenericPolynomial<N>>,
-) -> Polynomial<N>
-where
-    N: Zero + Copy + AddAssign + Neg<Output = N>,
-{
-    let mut sink = Polynomial::zero();
-    for (coeff, deg) in src_a.term_iter() {
-        sink.add_term(coeff, deg);
-    }
+#[macro_export]
+macro_rules! poly_mul {
+    ($lhs:expr, $rhs:expr) => {
+        {
+            let mut sink = Polynomial::zero();
+            poly_mul!($lhs, $rhs, sink)
+        }
+    };
 
-    for (coeff, deg) in src_b.term_iter() {
-        sink.add_term(-coeff, deg);
-    }
+    ($lhs:expr, $rhs:expr, $sink:expr) => {
+        {
+        for (coeff_a, deg_a) in $lhs.term_iter() {
+            for (coeff_b, deg_b) in $rhs.term_iter() {
+                $sink.add_term(coeff_a * coeff_b, deg_a + deg_b);
+            }
+        }
 
-    sink
+        $sink
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! poly_sub {
+    ($lhs:expr, $rhs:expr) => {
+        {
+            let mut sink = Polynomial::zero();
+            poly_sub!($lhs, $rhs, sink)
+        }
+    };
+
+    ($lhs:expr, $rhs:expr, $sink:expr) => {
+        {
+        for (coeff, deg) in $lhs.term_iter() {
+            $sink.add_term(coeff, deg);
+        }
+
+        for (coeff, deg) in $rhs.term_iter() {
+            $sink.add_term(-coeff, deg);
+        }
+
+        $sink
+        }
+    };
 }
